18d3cf8ad3c27af30db33d2734318cb23519ef5a
David Wilkie <dwilkie@gmail.com>
2016-06-03 09:56:19 +0700

    Add transfers

--- lib/tneakearyon/bank/maybank_cambodia/web_api.rb
+++ lib/tneakearyon/bank/maybank_cambodia/web_api.rb
@@ -3,24 +3,27 @@ require 'byebug' # REMOVE ME
 require 'rack/utils'
 
 class Tneakearyon::Bank::MaybankCambodia::WebApi < Tneakearyon::Bank::MaybankCambodia::Api
-  API_BASE_ENDPOINT        = 'https://www.maybank2u.com.kh'
+  API_BASE_ENDPOINT                 = 'https://www.maybank2u.com.kh'
 
-  API_PORTAL_ACCESS_PATH   = '/RIB/ib101/ibPortalAccess.do'
-  API_LOGIN_PATH           = '/RIB/ib101/ibLogin.do'
-  API_LOGIN_PASSWORD_PATH  = '/RIB/ib101/ibLoginPassword.do'
-  API_ACCOUNT_SUMMARY_PATH = '/RIB/ib102/ibAccountSummary.do'
-  API_ACCOUNT_INFO_PATH    = '/RIB/ib102/ibAccountInfo.do'
+  API_PORTAL_ACCESS_PATH                 = '/RIB/ib101/ibPortalAccess.do'
+  API_LOGIN_PATH                         = '/RIB/ib101/ibLogin.do'
+  API_LOGIN_PASSWORD_PATH                = '/RIB/ib101/ibLoginPassword.do'
+  API_ACCOUNT_SUMMARY_PATH               = '/RIB/ib102/ibAccountSummary.do'
+  API_ACCOUNT_INFO_PATH                  = '/RIB/ib102/ibAccountInfo.do'
+  API_THIRD_PARTY_TRANSFER_DETAILS_PATH  = '/RIB/ib104/ib3rdPartyTransferDetails.do'
+  API_THIRD_PARTY_TRANSFER_CONFIRM_PATH  = '/RIB/ib104/ib3rdPartyTransferConfirm.do'
 
   PAGE_ELEMENTS = {
-    :login_token_input_name             => "org.apache.struts.taglib.html.TOKEN",
+    :form_token_input_name             => "org.apache.struts.taglib.html.TOKEN",
     :login_name                         => "logdetailsbold",
-    :account_number_query_string_param  => "acctNo"
+    :account_number_query_string_param  => "acctNo",
+    :secondary_token_query_string_param => "SECONDARY_TOKEN"
   }
 
   WHITELISTED_COOKIE_NAMES = ["M2UMCI"]
 
   attr_accessor :username, :password, :account_details,
-                :cookies, :login_token, :encryption_key, :ib_links
+                :cookies, :form_token, :encryption_key, :html_response, :secondary_token
 
   def initialize(params = {})
     self.username = params[:username]
@@ -28,39 +31,78 @@ class Tneakearyon::Bank::MaybankCambodia::WebApi < Tneakearyon::Bank::MaybankCam
   end
 
   def fetch_account_details!
-    return account_details if logged_in?
-    login_response = login!
-
-    html = parse_html_response(login_response.body)
-
-    self.account_details ||= {}
-    set_login_details(html)
-
-    self.ib_links ||= {}
-    set_ib_link_account_summary(html)
-
+    login! if !logged_in?
     fetch_account_summary!
-
     account_details
+  end
+
+  def transfer!(options = {})
+    login! if !logged_in?
+    raise(ArgumentError, "You must pass from_account, :to_account and :amount") if !options.has_key?(:from_account) || !options.has_key?(:to_account) || !options.has_key?(:amount)
+    get_third_party_transfer_details!
+    post_third_party_transfer_confirm!(options)
   end
 
   private
 
+  def parse_url_query_string(url)
+    Rack::Utils.parse_query(URI.parse(url).query)
+  end
+
   def logged_in?
-    !!account_details && !account_details.empty?
+    !!login_name
   end
 
   def login!
     setup_session!
     post_username!
     post_password!
+    set_login_details
+  end
+
+  def get_third_party_transfer_details!
+    set_html_response(
+      HTTParty.get(
+        ib_third_party_transfer_details_endpoint,
+        :query => {
+          page_element(:secondary_token_query_string_param) => secondary_token,
+          "transferType" => "open"
+        },
+        :headers => set_headers
+      )
+    )
+    self.secondary_token = set_form_token(html_response)
+  end
+
+  def post_third_party_transfer_confirm!(options = {})
+    HTTParty.post(
+      ib_third_party_transfer_confirm_endpoint,
+      :body => set_third_party_transfer_request_body(options),
+      :headers => set_headers
+    )
+  end
+
+  def set_third_party_transfer_request_body(options = {})
+    body = {}
+    body.merge!(
+      "fromAcct" => options[:from_account],
+      "toAcct" => options[:to_account],
+      "amount" => options[:amount].to_s,
+      page_element(:secondary_token_query_string_param) => secondary_token,
+      page_element(:form_token_input_name) => secondary_token
+    )
+
+    effective_date = Date.parse(((options[:effective_date] && options[:effective_date]) || Date.today).to_s)
+    body.merge!("effectiveDate" => effective_date.strftime("%Y%m%d"))
+    body.merge!("email" => options[:email]) if options[:email]
+    body
   end
 
   def setup_session!
     response = HTTParty.get(ib_portal_access_endpoint)
     set_cookies(response)
     html = parse_html_response(response.body)
-    set_login_token(html)
+    set_form_token(html)
     set_encryption_key(html)
   end
 
@@ -73,29 +115,43 @@ class Tneakearyon::Bank::MaybankCambodia::WebApi < Tneakearyon::Bank::MaybankCam
   end
 
   def post_password!
-    HTTParty.post(
-      ib_login_password_endpoint,
-      :body => set_login_request_body(
-        "password" => des_encrypt(password)
-      ),
-      :headers => set_headers
+    set_html_response(
+      HTTParty.post(
+        ib_login_password_endpoint,
+        :body => set_login_request_body(
+          "password" => des_encrypt(password)
+        ),
+        :headers => set_headers
+      )
     )
   end
 
-  def set_ib_link_account_summary(html)
-    ib_links[:account_summary] ||= html.at_xpath("//a[contains(@href, '#{API_ACCOUNT_SUMMARY_PATH}')]")["href"]
+  def set_html_response(raw_response)
+    self.html_response = parse_html_response(raw_response.body)
+    reset_secondary_token
+    html_response
+  end
+
+  def reset_secondary_token
+    if page_element = html_response.at_xpath("//a[contains(@href, '#{page_element(:secondary_token_query_string_param)}')]")
+      self.secondary_token = parse_url_query_string(
+        page_element["href"]
+      )[page_element(:secondary_token_query_string_param)]
+    end
   end
 
   def fetch_account_summary!
     accounts = self.account_details[:accounts] ||= {}
+    account_summary_path = html_response.at_xpath("//a[contains(@href, '#{API_ACCOUNT_SUMMARY_PATH}')]")["href"]
 
-    response = HTTParty.get(api_endpoint(ib_links[:account_summary]), :headers => set_headers)
-    html = parse_html_response(response.body)
+    set_html_response(
+      HTTParty.get(api_endpoint(account_summary_path), :headers => set_headers)
+    )
 
     account_info_link_xpath = "//a[contains(@href, '#{API_ACCOUNT_INFO_PATH}')]"
 
-    html.at_xpath(account_info_link_xpath).ancestors("tbody").first.search("tr").each do |row|
-      account_number = Rack::Utils.parse_query(URI.parse(row.at_xpath(account_info_link_xpath)["href"]).query)[page_element(:account_number_query_string_param)]
+    html_response.at_xpath(account_info_link_xpath).ancestors("tbody").first.search("tr").each do |row|
+      account_number = parse_url_query_string(row.at_xpath(account_info_link_xpath)["href"])[page_element(:account_number_query_string_param)]
       columns = row.search("td")
       accounts[account_number] = {
         :number => account_number,
@@ -105,13 +161,22 @@ class Tneakearyon::Bank::MaybankCambodia::WebApi < Tneakearyon::Bank::MaybankCam
     end
   end
 
-  def set_login_details(html)
-    self.account_details[:login_name] = html.at_xpath("//*[contains(@class, '#{page_element(:login_name)}')]").text
+  def login_name
+    (account_details || {})[:login_name]
+  end
+
+  def set_login_details
+    if page_element = html_response.at_xpath("//*[contains(@class, '#{page_element(:login_name)}')]")
+      self.account_details ||= {}
+      self.account_details[:login_name] = page_element.text
+    else
+      raise(RuntimeError, "Unable to login. Check credentials")
+    end
   end
 
   def set_login_request_body(params = {})
     {
-      page_element(:login_token_input_name) => login_token,
+      page_element(:form_token_input_name) => form_token,
       "userName" => des_encrypt(username),
     }.merge(params)
   end
@@ -161,6 +226,14 @@ class Tneakearyon::Bank::MaybankCambodia::WebApi < Tneakearyon::Bank::MaybankCam
     api_endpoint(API_LOGIN_PASSWORD_PATH)
   end
 
+  def ib_third_party_transfer_details_endpoint
+    api_endpoint(API_THIRD_PARTY_TRANSFER_DETAILS_PATH)
+  end
+
+  def ib_third_party_transfer_confirm_endpoint
+    api_endpoint(API_THIRD_PARTY_TRANSFER_CONFIRM_PATH)
+  end
+
   def api_endpoint(path)
     api_base_endpoint + path
   end
@@ -173,8 +246,8 @@ class Tneakearyon::Bank::MaybankCambodia::WebApi < Tneakearyon::Bank::MaybankCam
     Nokogiri::HTML(response_body)
   end
 
-  def set_login_token(html)
-    self.login_token = (html.at_xpath(".//input[@name='#{page_element(:login_token_input_name)}']") || {})["value"]
+  def set_form_token(html)
+    self.form_token = (html.at_xpath(".//input[@name='#{page_element(:form_token_input_name)}']") || {})["value"]
   end
 
   def set_encryption_key(html)
@@ -186,5 +259,7 @@ end
 
 # maybank = Tneakearyon::Bank::MaybankCambodia::WebApi.new(:username => "username", :password => "password")
 # maybank.fetch_account_details!
+# response = maybank.transfer!(:from_account => "fromAccount", :to_account => "toAccount", :amount => Money.new(10000, "USD"), :email => "someone@gmail.com")
+# File.open("login_output.html", 'w') { |file| file.write(maybank.html_response.to_s) }
 # File.open("login_output.html", 'w') { |file| file.write(response.body) }
 # html = Nokogiri::HTML(response.body)
